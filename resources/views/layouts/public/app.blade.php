13090c6cc11d802471e2dfb18f611ef5a92da8e3
tobias <tobiasmikha33@gmail.com>
2025-10-19 23:13:38 +0700

    Not yet finished

--- resources/views/layouts/public/app.blade.php
+++ resources/views/layouts/public/app.blade.php
@@ -176,18 +176,23 @@
     <script src="{{ asset('files\assets\pages\widget\amchart\pie.js') }}"></script>
     <script src="{{ asset('files\assets\pages\widget\amchart\light.js') }}"></script>
     <!-- data-table js -->
-    <script src="{{ asset('files\bower_components\datatables.net\js\jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('files\bower_components\datatables.net-buttons\js\dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('files\assets\pages\data-table\js\jszip.min.js') }}"></script>
-    <script src="{{ asset('files\assets\pages\data-table\js\pdfmake.min.js') }}"></script>
-    <script src="{{ asset('files\assets\pages\data-table\js\vfs_fonts.js') }}"></script>
-    <script src="{{ asset('files\bower_components\datatables.net-buttons\js\buttons.print.min.js') }}"></script>
-    <script src="{{ asset('files\bower_components\datatables.net-buttons\js\buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('files\bower_components\datatables.net-bs4\js\dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('files\bower_components\datatables.net-responsive\js\dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('files\bower_components\datatables.net-responsive-bs4\js\responsive.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('files\assets\pages\data-table\js\data-table-custom.js') }}"></script>
+    <script src="{{asset('files\bower_components\datatables.net\js\jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('files\bower_components\datatables.net-buttons\js\dataTables.buttons.min.js')}}"></script>
+    <script src="{{asset('files\assets\pages\data-table\js\jszip.min.js')}}"></script>
+    <script src="{{asset('files\assets\pages\data-table\js\pdfmake.min.js')}}"></script>
+    <script src="{{asset('files\assets\pages\data-table\js\vfs_fonts.js')}}"></script>
+    <script src="{{asset('files\assets\pages\data-table\extensions\buttons\js\dataTables.buttons.min.js')}}"></script>
+    <script src="{{asset('files\assets\pages\data-table\extensions\buttons\js\buttons.flash.min.js')}}"></script>
+    <script src="{{asset('files\assets\pages\data-table\extensions\buttons\js\jszip.min.js')}}"></script>
+    <script src="{{asset('files\assets\pages\data-table\extensions\buttons\js\vfs_fonts.js')}}"></script>
+    <script src="{{asset('files\assets\pages\data-table\extensions\buttons\js\buttons.colVis.min.js')}}"></script>
+    <script src="{{asset('files\bower_components\datatables.net-buttons\js\buttons.print.min.js')}}"></script>
+    <script src="{{asset('files\bower_components\datatables.net-buttons\js\buttons.html5.min.js')}}"></script>
+    <script src="{{asset('files\bower_components\datatables.net-bs4\js\dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('files\bower_components\datatables.net-responsive\js\dataTables.responsive.min.js')}}"></script>
+    <script src="{{asset('files\bower_components\datatables.net-responsive-bs4\js\responsive.bootstrap4.min.js')}}"></script>
     <!-- Custom js -->
+    <script src="{{asset('files/assets/pages/data-table/extensions/buttons/js/extension-btns-custom.js')}}"></script>
     <script src="{{ asset('files\assets\js\pcoded.min.js') }}"></script>
     <script src="{{ asset('files\assets\js\vartical-layout.min.js') }}"></script>
     <script src="{{ asset('files\assets\js\jquery.mCustomScrollbar.concat.min.js') }}"></script>
